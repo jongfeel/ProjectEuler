0fc391bd44feddc390b6ca213978701f836a7d2e
Kim Jong Feel <DeveloperFEEL@gmail.com>
2018-08-16 02:37:13 +0900

    Solve Problem10 and add solve descriptions

--- Problems/Problem10/Problem10/Program.vb
+++ Problems/Problem10/Problem10/Program.vb
@@ -1,10 +1,23 @@
-Imports System
-
 Module Program
     Sub Main(args As String())
-        Dim SumOfPrimeNumber As ULong = 0
+        'Dim StartTime As DateTime = DateTime.Now
+        Dim UntilNumber As Integer = 2000000
+        Dim SumOfThePrimes As ULong = 0
+        Dim Primes As List(Of Long) = Enumerable.Range(0, UntilNumber).Select(Of Long)(Function(p) p).ToList()
 
-        Console.WriteLine(SumOfPrimeNumber)
+        Primes(1) = 0   '1 is not prime
+        For index = 2 To Math.Sqrt(UntilNumber)
+            If Primes(index) <> 0 Then
+                For innerIndex = index * 2 To Primes.Count - 1 Step index
+                    Primes(innerIndex) = 0
+                Next
+            End If
+        Next
+
+        Console.WriteLine(Primes.Sum())
+        'Dim EndTime As DateTime = DateTime.Now
+        'Dim ElapsedTime As TimeSpan = EndTime - StartTime
+        'Console.WriteLine(ElapsedTime.TotalSeconds)
         Console.ReadLine()
 
     End Sub
