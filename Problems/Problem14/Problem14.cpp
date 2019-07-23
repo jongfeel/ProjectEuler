207e10c9f779e6d04eeccd44e186edfa7d6aee46
Kim Jong Feel <DeveloperFEEL@gmail.com>
2019-07-24 01:20:34 +0900

    Update readme.md and code fix: count type change to size_t

--- Problems/Problem14/Problem14.cpp
+++ Problems/Problem14/Problem14.cpp
@@ -1,24 +1,27 @@
 #include <iostream>
 #include <algorithm>
+#include <ctime>
 
 using namespace std;
 
 int main() 
 {
+    clock_t begin = clock();
+
     int input;
     int largestCount = 0;
-    int resultNumber = 2;
+    size_t resultNumber = 2;
 
     // start number is 2
-    for (size_t i = 2; i <= 1000000; i++)
+    for (size_t i = 2; i < 1000000; i++)
     {
-        int number = i;
-        int count = 0;
+        size_t number = i;
+        int count = 1;
         while (number != 1)
         {
             if (number % 2 == 0)
             {
-                number /= 2;
+                number = number / 2;
             }
             else
             {
@@ -31,11 +34,15 @@ int main()
             largestCount = count;
             resultNumber = i;
         }
-        // cout << i << ": count is " << count << endl;
+        //cout << i << ": count is " << count << endl;
     }
+
+    clock_t end = clock();
+    double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
 
     cout << "Largest count: " << largestCount << endl;
     cout << "Result number: " << resultNumber << endl;
+    cout << "Elapsed time: " << elapsed_secs << "ms" << endl;
     
     cout << "To end of program, input any character and enter...";
     cin >> input;
