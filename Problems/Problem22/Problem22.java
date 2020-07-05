29aeff0c71c2b704d2a6cfbd1d32abf152e4984b
jongfeel <DeveloperFEEL@gmail.com>
2020-07-05 12:10:24 +0900

    Solve problem 22 source, first code for easy way

--- Problems/Problem22/Problem22.java
+++ Problems/Problem22/Problem22.java
@@ -1,7 +1,42 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Problem22 {
-    
+
     public static void main(final String[] args) {
-        
+
         System.out.println("Problem22");
+
+        try {
+            String fileContents = Files.readString(Path.of("p022_names.txt"));
+            // System.out.println(fileContents);
+
+            String removedDoubeQuote = fileContents.replace("\"", "");
+            // System.out.println(removedDoubeQuote);
+
+            String[] names = removedDoubeQuote.split(",");
+            // System.out.println(names.length);
+            
+            List<String> nameList = Arrays.asList(names);
+            Collections.sort(nameList);
+
+            int index = 1;
+            int namesSum = 0;
+            for (String name : nameList) {
+                int nameSum = 0;
+                for (int i = 0; i < name.length(); i++) {
+                    nameSum += name.charAt(i) - 64;
+                }
+                namesSum += nameSum * index;
+                index++;
+            }
+
+            System.out.println(namesSum);
+
+        } catch (Exception e) {
+        }
     }
 }
