0c314a745ca5f19eacf07a225cd09f11839b92ea
jongfeel <DeveloperFEEL@gmail.com>
2020-08-30 20:10:27 +0900

    Add problem23 source

--- Problems/Problem23/Problem23.cpp
+++ Problems/Problem23/Problem23.cpp
@@ -1,13 +1,51 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <numeric>
 
 using namespace std;
 
+bool IsAbundant(int number)
+{
+    int sumOfProperDivisors = 0;
+    for (int i = 1; i <= number / 2; i++)
+    {
+        if (number % i == 0)
+        {
+            sumOfProperDivisors += i;
+        }
+    }
+    return sumOfProperDivisors > number;
+}
+
 int main() 
 {
-    int input;
+    vector<int> NotSumOfTwoAbundantNumbers;
+    for (int i = 0; i <= 28123; i++)
+    {
+        NotSumOfTwoAbundantNumbers.push_back(i);
+    }
 
+    vector<int> AbundantNumbers;
+    for (int i = 12; i <= 28123; i++)
+    {
+        if (IsAbundant(i))
+        {
+            AbundantNumbers.push_back(i);
+            for (int abundantNumber : AbundantNumbers)
+            {
+                int sumOfTwoAbundantNumber = abundantNumber + i;
+                if (sumOfTwoAbundantNumber <= 28123)
+                {
+                    NotSumOfTwoAbundantNumbers[sumOfTwoAbundantNumber] = 0;
+                }
+            }
+        }        
+    }
+
+    int sum = std::accumulate(NotSumOfTwoAbundantNumbers.begin(), NotSumOfTwoAbundantNumbers.end(), 0);
+    cout << sum << endl;
+
+    int input;
     cout << "To end of program, input any character and enter..." << endl;
     cin >> input;
     return 0;
